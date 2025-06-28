96e724572d8d29d7d11d78dcd8549746c26eb229
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2025-06-28 19:09:19 +0800

    landing page | header | mobile and web view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/landing-page', pathMatch: 'full' }, // Default to '/home'
+  { path: 'landing-page', component: LandingPageComponent}
+];
